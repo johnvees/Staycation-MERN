cef7ca1857c8e886d46203d063581f516b76a0ea
johnvees <yohanesvelly23@gmail.com>
2022-02-20 17:31:51 +0700

    Update Component Header and scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import LandingPage from 'pages/LandingPage';
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './assets/scss/style.scss';
 
 function App() {
-  return <div className="App">hai dunia!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import moduleName from 'elements/Button'
+import Header from 'parts/Header';
 export default class LandingPage extends Component {
   render() {
-    return (
-      <div>LandingPage</div>
-    )
+    return <>
+      <Header {...this.props}></Header>
+    </>;
   }
 }
