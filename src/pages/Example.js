d037b9395f04c11247cc2b5c2563b953fa10c7d5
johnvees <yohanesvelly23@gmail.com>
2022-02-28 15:39:44 +0700

    add input date component and format date utils

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import InputNumber from 'elements/Form/InputNumber';
+import { InputDate } from 'elements/Form';
 export default class Example extends Component {
   state = {
-    value: '1',
+    value: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    },
   };
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({ value: e.target.value });
   };
   render() {
     console.log(this.state);
@@ -16,10 +20,8 @@ export default class Example extends Component {
           style={{ height: '100vh' }}
         >
           <div className="col-auto">
-            <InputNumber
+            <InputDate
               max={30}
-              suffix=" night"
-              isSuffixPlural
               onChange={this.handleChange}
               name="value"
               value={this.state.value}
