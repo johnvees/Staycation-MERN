1f08e6320bca6089ecb1de51fd3fa050ed9c1229
johnvees <yohanesvelly23@gmail.com>
2022-02-28 13:18:37 +0700

    import detail page images and add input number component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import LandingPage from 'pages/LandingPage';
+import Example from 'pages/Example';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route path="/" component={LandingPage} />
+        <Route path="/example" component={Example} />
       </Router>
     </div>
   );
